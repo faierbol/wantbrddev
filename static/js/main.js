398d24178af4eb52ddb71b883e51e32f3c47848c
Luke Holdsworth <iamholdsworth@gmail.com>
2018-05-12 19:09:39 +0100

    missing static

---
+++ static/js/main.js
@@ -0,0 +1,20 @@
+$(function() {
+    $('.match').matchHeight();
+});
+
+// sticky header
+function init() {
+    window.addEventListener('scroll', function(e){
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+            shrinkOn = 100,
+            header = document.querySelector("header#main");
+        if (distanceY > shrinkOn) {
+            classie.add(header,"shrink");
+        } else {
+            if (classie.has(header,"shrink")) {
+                classie.remove(header,"shrink");
+            }
+        }
+    });
+}
+window.onload = init();
