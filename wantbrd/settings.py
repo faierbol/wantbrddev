ae66dbb487afc4e896c0c151aad3677f1480541b
Luke Holdsworth <iamholdsworth@gmail.com>
2018-06-16 20:28:33 +0100

    meh

--- wantbrd/settings.py
+++ wantbrd/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = '3i22t4vm=tnjqp%w@ye^n=zo=z$12=f=)bi_sw0ij9a2mup(o-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', '0.0.0.0']
 
 
 # Application definition
