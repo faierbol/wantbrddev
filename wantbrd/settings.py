edcd3e1c49aeaf55821e838d69b405682fc0c6e9
Luke Holdsworth <iamholdsworth@gmail.com>
2018-08-02 20:25:57 +0100

    Font awesome

--- wantbrd/settings.py
+++ wantbrd/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = '3i22t4vm=tnjqp%w@ye^n=zo=z$12=f=)bi_sw0ij9a2mup(o-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', '0.0.0.0']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', '0.0.0.0', 'localhost']
 CORS_ORIGIN_ALLOW_ALL=True
 
 # Application definition
