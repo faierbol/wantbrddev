f165326dde13a5924618303babc851ce5c510f7c
Luke Holdsworth <iamholdsworth@gmail.com>
2019-01-07 19:38:57 +0000

    fixes

--- board/views.py
+++ board/views.py
@@ -58,10 +58,10 @@ def home(request):
 	template = 'index.html'
 	hot_items = []
 
-	trending_items = get_trending_items(request,30)
-	trending_users = get_trending_users(request,30)
-	trending_boards = get_trending_boards(request,30)
-	recommended_boards = get_recommended_boards(request)
+	trending_items = get_trending_items(request,7)
+	trending_users = get_trending_users(request,7)
+	trending_boards = get_trending_boards(request,7)
+	# recommended_boards = get_recommended_boards(request)
 
 	mixed = list(trending_boards) + list(trending_items) + list(trending_users) + list(recommended_boards)
 	random.shuffle(mixed)	
